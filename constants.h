8815022f25ce84d16bc170d1e2c87ef1206701af
di-was <diwasdahal109@gmail.com>
2023-11-26 18:03:18 +0545

    New variables defined

--- constants.h
+++ constants.h
@@ -1,4 +1,7 @@
 #define FALSE 0
 #define TRUE 1
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 800
+#define BLOCK_WIDTH 25
+#define BLOCK_HEIGHT 25
+
